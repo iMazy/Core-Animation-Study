e020b6ebdaa7e7e1479a13fd3fb5dc3a1350dd90
Mazy <mazongyong@163.com>
2017-05-04 17:25:25 +0800

    feat: 添加旋转、缩放，平移变换

--- CGAffineTransform/CGAffineTransform/ViewController.swift
+++ CGAffineTransform/CGAffineTransform/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageViewA: UIImageView!
+    
+    @IBOutlet weak var imageViewB: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imageViewC: UIImageView!
+    
+    @IBOutlet weak var imageViewD: UIImageView!
+    
+    var angle: CGFloat = 0
+    var scale: CGFloat = 1
+    
+    @IBAction func imageViewA_Action() {
+        angle = angle + CGFloat(M_PI_4)
+        let transform = CGAffineTransform(rotationAngle: angle)
+        imageViewA.layer.setAffineTransform(transform)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func imageViewB_Action() {
+        
+        scale = scale < 0  ? scale + 0.1 : scale - 0.1
+        
+        let transform = CGAffineTransform(scaleX: scale, y: scale)
+        imageViewB.layer.setAffineTransform(transform)
     }
-
+    
+    @IBAction func imageViewC_Action() {
+        let transform = CGAffineTransform(translationX: 50, y: 50)
+        imageViewC.layer.setAffineTransform(transform)
+    }
+    
+    /// 混合变换
+    @IBAction func imageViewD_Action() {
+        
+        let transform = CGAffineTransform.identity
+        
+        transform.rotated(by: CGFloat(M_PI_4))
+        transform.scaledBy(x: 0.5, y: 0.5)
+        transform.translatedBy(x: 100, y: 50)
+        
+        imageViewD.layer.setAffineTransform(transform)
+    }
 
 }
 
