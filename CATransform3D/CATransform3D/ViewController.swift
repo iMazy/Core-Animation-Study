2e25bdc965baa89fd1a3d10e9cff04d9808c20f9
Mazy <mazongyong@163.com>
2017-05-05 10:08:34 +0800

    feat；初始化界面

--- CATransform3D/CATransform3D/ViewController.swift
+++ CATransform3D/CATransform3D/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var imageViewA: UIImageView!
+    
+    @IBOutlet weak var imageViewB: UIImageView!
+    
+    @IBOutlet weak var imageViewC: UIImageView!
+    
+    @IBOutlet weak var imageViewD: UIImageView!
+    
+    @IBAction func rotationAction() {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func scaleAction() {
+        
     }
-
+   
+    @IBAction func translationAction() {
+        
+    }
+    
+    @IBAction func multyAction() {
+        
+    }
+    
 
 }
 
