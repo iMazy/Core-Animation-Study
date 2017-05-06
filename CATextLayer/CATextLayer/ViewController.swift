d076574c252b4618359355689d7bab7d563bac78
Mazy <mazongyong@163.com>
2017-05-06 22:59:41 +0800

    feat: 初始化文本图层

---
+++ CATextLayer/CATextLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CATextLayer
+//
+//  Created by  Mazy on 2017/5/6.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
