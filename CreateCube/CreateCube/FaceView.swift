73e39f5f7dac47900904b7084b2de671e3f2919b
Mazy <mazongyong@163.com>
2017-05-08 10:15:31 +0800

    feat：添加6个UIView

---
+++ CreateCube/CreateCube/FaceView.swift
@@ -0,0 +1,22 @@
+
+//
+//  FaceView.swift
+//  CreateCube
+//
+//  Created by  Mazy on 2017/5/8.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
