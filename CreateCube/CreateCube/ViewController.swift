ec78fea9e0f392a954c46891b794d37dd05998f6
Mazy <mazongyong@163.com>
2017-05-08 15:27:31 +0800

    feat: 完成立方体的显示

--- CreateCube/CreateCube/ViewController.swift
+++ CreateCube/CreateCube/ViewController.swift
@@ -10,14 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    var faces: [FaceView] = [FaceView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let face1 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[0] as! FaceView
+        let face2 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[1] as! FaceView
+        let face3 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[2] as! FaceView
+        let face4 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[3] as! FaceView
+        let face5 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[4] as! FaceView
+        let face6 = Bundle.main.loadNibNamed("FaceView", owner: nil, options: nil)?[5] as! FaceView
+        
+        faces.append(contentsOf: [face1,face2,face3,face4,face5,face6])
+        
+        var perspective = CATransform3DIdentity
+        perspective.m34 = -1.0/500
+        perspective = CATransform3DRotate(perspective, CGFloat(-M_PI_4), 1, 0, 0)
+        perspective = CATransform3DRotate(perspective, CGFloat(-M_PI_4), 0, 1, 0)
+        containerView.layer.sublayerTransform = perspective
+        
+        let distance: CGFloat = 100
+        
+        // add cube face1
+        var transform = CATransform3DMakeTranslation(0, 0, distance)
+        addFace(with: face1, transform: transform)
+        
+        // add cube face2
+        transform = CATransform3DMakeTranslation(distance, 0, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI_2), 0, 1, 0)
+        addFace(with: faces[1], transform: transform)
+        
+        // add cube face3
+        transform = CATransform3DMakeTranslation(0, -distance, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI_2), 1, 0, 0)
+        addFace(with: face3, transform: transform)
+        
+        // add cube face4
+        transform = CATransform3DMakeTranslation(0, distance, 0)
+        transform = CATransform3DRotate(transform, CGFloat(-M_PI_2), 1, 0, 0)
+        addFace(with: face4, transform: transform)
+        
+        // add cube face5
+        transform = CATransform3DMakeTranslation( -distance, 0, 0)
+        transform = CATransform3DRotate(transform, CGFloat(-M_PI_2), 0, 1, 0)
+        addFace(with: face5, transform: transform)
+        
+        // add cube face6
+        transform = CATransform3DMakeTranslation(0, 0, -distance)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI), 0, 1, 0)
+        addFace(with: face6, transform: transform)
+        
+        
     }
+    
+    func addFace(with face: FaceView,transform: CATransform3D) {
+        
+        face.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        
+        containerView.addSubview(face)
+        
+        let size = containerView.bounds.size
+        
+        face.center = CGPoint(x: size.width/2, y: size.height/2)
+        
+        face.layer.transform = transform
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
