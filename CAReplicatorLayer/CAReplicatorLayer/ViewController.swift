3f08e12b88229ec0b676a7785f3614e57c0d2390
Mazy <mazongyong@163.com>
2017-05-09 15:33:40 +0800

    feat: 完成图层的复制和旋转

--- CAReplicatorLayer/CAReplicatorLayer/ViewController.swift
+++ CAReplicatorLayer/CAReplicatorLayer/ViewController.swift
@@ -10,14 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var indicitorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // create replicator layer and add it to containerView
+        let replicator = CAReplicatorLayer()
+        replicator.frame = containerView.bounds
+        containerView.layer.addSublayer(replicator)
+        
+        
+        let width: CGFloat = 70
+        // config the replicator
+        replicator.instanceCount = 10
+        
+        var transform = CATransform3DIdentity
+        transform = CATransform3DTranslate(transform, 0, width*2, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI/5), 0, 0, 1)
+        transform = CATransform3DTranslate(transform, 0, -2*width, 0)
+        replicator.instanceTransform = transform
+        
+        replicator.instanceBlueOffset = -0.1
+        replicator.instanceGreenOffset = -0.1
+        
+        // create a sublayer and place it inside the replicator
+        let layer = CALayer()
+        layer.frame = CGRect(x: 0, y: 0, width: width, height: width)
+        layer.backgroundColor = UIColor.white.cgColor
+        replicator.addSublayer(layer)
+        
+        
+        addCircle()
     }
+    
+    func addCircle() {
+        let replicator = CAReplicatorLayer()
+        replicator.frame = indicitorView.bounds
+        indicitorView.layer.addSublayer(replicator)
+        
+        
+        let width: CGFloat = 20
+        // config the replicator
+        replicator.instanceCount = 60
+        
+        var transform = CATransform3DIdentity
+        transform = CATransform3DTranslate(transform, 0, width*3, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI/30), 0, 0, 1)
+        transform = CATransform3DTranslate(transform, 0, -3*width, 0)
+        replicator.instanceTransform = transform
+        
+        replicator.instanceBlueOffset = -1/60
+        replicator.instanceGreenOffset = -1/60
+        
+        // create a sublayer and place it inside the replicator
+        let layer = CALayer()
+        layer.frame = CGRect(x: 0, y: 0, width: width, height: width)
+        layer.backgroundColor = UIColor.white.cgColor
+        replicator.addSublayer(layer)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
