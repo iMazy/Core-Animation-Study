76da5c9255dbf17d3b816b292e72720a5e34a505
Mazy <mazongyong@163.com>
2017-05-09 16:00:58 +0800

    feat： 用CAReplicatorLayer完成镜像

---
+++ CAReplicatorLayer/CAReplicatorLayer/ReflectionView.swift
@@ -0,0 +1,53 @@
+//
+//  ReflectionView.swift
+//  CAReplicatorLayer
+//
+//  Created by  Mazy on 2017/5/9.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ReflectionView: UIView {
+
+    override class var layerClass: AnyClass {
+        get {
+            return CAReplicatorLayer.self
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    
+    func setup() {
+    
+        // config repliactor
+        let newLayer = self.layer as! CAReplicatorLayer
+        newLayer.instanceCount = 2
+        
+        var transform = CATransform3DIdentity
+        let verticalOffset = bounds.size.height - 30
+        transform = CATransform3DTranslate(transform, 0, verticalOffset, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI_4), 1, 0, 0)
+        transform = CATransform3DScale(transform, 1, -1, 0)
+        newLayer.instanceTransform = transform
+        
+        newLayer.instanceAlphaOffset = -0.6
+        
+        
+    }
+
+}
