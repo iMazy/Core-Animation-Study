f679f6a2ecc8c6684dc325de0ae424015fbedb3d
Mazy <mazongyong@163.com>
2017-05-10 09:57:21 +0800

    faet: 完成类似于UIScrollView的滚动效果

---
+++ CAScrollLayer/CAScrollLayer/ScrollView.swift
@@ -0,0 +1,66 @@
+//
+//  ScrollView.swift
+//  CAScrollLayer
+//
+//  Created by  Mazy on 2017/5/10.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ScrollView: UIView {
+
+    override class var layerClass: AnyClass {
+        get {
+          return CAScrollLayer.self
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        setup()
+//    }
+    
+    func setup() {
+        // enable clipping
+        layer.masksToBounds = true
+        
+        // attach pan gesture recognizer
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        addGestureRecognizer(panGesture)
+    }
+    
+    func panAction(recognizer: UIPanGestureRecognizer)  {
+        
+        var offset: CGPoint = bounds.origin
+        
+        switch recognizer.state {
+        case .changed:
+            
+            offset.x = -recognizer.translation(in: self).x
+            offset.y = -recognizer.translation(in: self).y
+            
+            let newLayer = layer as! CAScrollLayer
+            newLayer.scroll(to: offset)
+            
+            newLayer.scrollRectToVisible(self.bounds)
+            
+        case .ended:
+                recognizer.setTranslation(CGPoint.zero, in: self)
+            
+        default: break
+        }
+        
+    }
+
+}
