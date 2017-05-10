5da4692b50f39349d84ef8b18955240f9a7d51f4
Mazy <mazongyong@163.com>
2017-05-10 15:19:50 +0800

    feat: 完成粒子爆炸效果的展示

--- CAEmitterLayer/CAEmitterLayer/ViewController.swift
+++ CAEmitterLayer/CAEmitterLayer/ViewController.swift
@@ -8,16 +8,101 @@
 
 import UIKit
 
+
+enum CellDirection {
+    case TopLeft
+    case TopRight
+    case BottomLeft
+    case BottomRight
+}
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var containerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        centerCells()
+        
+        createCell(with: .TopLeft)
+        createCell(with: .TopRight)
+        createCell(with: .BottomLeft)
+        createCell(with: .BottomRight)
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func centerCells() {
+        let emitterLayer = CAEmitterLayer()
+        // set frame
+        emitterLayer.frame = containerView.bounds
+        containerView.layer.addSublayer(emitterLayer)
+        
+        // config emitter
+        emitterLayer.renderMode = kCAEmitterLayerAdditive //
+        //
+        emitterLayer.emitterPosition = CGPoint(x: emitterLayer.bounds.width/2, y: emitterLayer.bounds.height/2)
+        
+        // create cell
+        let cell = CAEmitterCell()
+        // 如果是图片 必须是CIImage
+        cell.contents = UIImage(named: "interaction-spark")?.cgImage
+        // config cell
+        cell.birthRate = 30 // 产生的数量
+        cell.lifetime = 5.0 // 存活时间
+        cell.color = UIColor(colorLiteralRed: 1.0, green: 0.5, blue: 0.1, alpha: 1.0).cgColor // cell 的颜色
+        cell.alphaSpeed = -0.4 // 透明度降低率
+        cell.velocity = 50
+        cell.velocityRange = 50
+        cell.emissionRange = CGFloat(M_PI * 2) // 出现范围
+        
+        emitterLayer.emitterCells = [cell] // 添加到leyer中
+    }
+    
+    func createCell(with direction:CellDirection) {
+        
+        let emitterLayer = CAEmitterLayer()
+        // set frame
+//        emitterLayer.frame = rect
+        view.layer.addSublayer(emitterLayer)
+        
+        //        emitterLayer.emitterSize = CGSize(width: 20, height: 20)
+        // config emitter
+        emitterLayer.renderMode = kCAEmitterLayerAdditive //
+        
+        // create cell
+        let cell = CAEmitterCell()
+        // 如果是图片 必须是CIImage
+        cell.contents = UIImage(named: "interaction-spark")?.cgImage
+        // config cell
+        cell.birthRate = 30 // 产生的数量
+        cell.lifetime = 5.0 // 存活时间
+        cell.color = UIColor(colorLiteralRed: 1.0, green: 0.5, blue: 0.1, alpha: 1.0).cgColor // cell 的颜色
+        cell.alphaSpeed = -0.4 // 透明度降低率
+        cell.velocity = 50
+        cell.velocityRange = 50
+        
+        cell.emissionRange = CGFloat(M_PI_4) // 出现范围
+        
+        switch direction {
+        case .TopLeft:
+            emitterLayer.emitterPosition = CGPoint(x: 50, y: 50)
+            cell.emissionLongitude = 1
+        case .TopRight:
+            emitterLayer.emitterPosition = CGPoint(x: view.bounds.width-50, y: 50)
+            cell.emissionLongitude = 2
+        case .BottomLeft:
+            emitterLayer.emitterPosition = CGPoint(x: 50, y: view.bounds.height-50)
+            cell.emissionLongitude = -1
+        case .BottomRight:
+            emitterLayer.emitterPosition = CGPoint(x: view.bounds.width-50, y: view.bounds.height-50)
+            cell.emissionLongitude = -2
+        }
+        
+        emitterLayer.emitterCells = [cell] // 添加到leyer中
     }
 
 
