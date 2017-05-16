f7459ab2f577b31b85ddcf2af4ecb90c975030de
Mazy <mazy_ios@163.com>
2017-05-16 14:36:33 +0800

    feat: 完成过渡动画展示

--- CATransition/CATransition/ViewController.swift
+++ CATransition/CATransition/ViewController.swift
@@ -8,16 +8,104 @@
 
 import UIKit
 
+/*
+ CATransition
+ 
+ CAAnimation的子类
+ 
+ 
+ CATransition有一个type和subtype来标识变换效果。
+     type属性是一个NSString类型，可以被设置成如下类型：
+         TransitionFade
+         kCATransitionMoveIn
+         kCATransitionPush
+         kCATransitionReveal
+ 
+     后面三种过渡类型都有一个默认的动画方向，它们都从左侧滑入
+     通过subtype来控制它们的方向
+         kCATransitionFromRight
+         kCATransitionFromLeft
+         kCATransitionFromTop
+         kCATransitionFromBottom
+ 
+ 
+ */
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var styleControl: UISegmentedControl!
+    
+    @IBOutlet weak var subStyleControl: UISegmentedControl!
+    
+    var imageArray: [UIImage]?
+    
+    lazy var transition = CATransition()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let image_meet = UIImage(named: "guide_meet"),
+           let image_matched = UIImage(named: "guide_matched"),
+           let image_like =  UIImage(named: "guide_liked") {
+            
+            imageView.image = image_meet
+            
+            imageArray = [image_like,image_meet,image_matched]
+        }
+        
+        styleControl.addTarget(self, action: #selector(styleChange), for: .valueChanged)
+        
+        subStyleControl.addTarget(self, action: #selector(subStyleChange), for: .valueChanged)
+        
+    }
+    
+    func styleChange(segment: UISegmentedControl) {
+        
+        switch segment.selectedSegmentIndex {
+        case 0:
+            transition.type = kCATransitionFade
+        case 1:
+            transition.type = kCATransitionMoveIn
+        case 2:
+            transition.type = kCATransitionPush
+        case 3:
+            transition.type = kCATransitionReveal
+        default:
+            break
+        }
+    }
+    
+    func subStyleChange(segment: UISegmentedControl) {
+        switch segment.selectedSegmentIndex {
+        case 0:
+            transition.subtype = kCATransitionFromLeft
+        case 1:
+            transition.subtype = kCATransitionFromRight
+        case 2:
+            transition.subtype = kCATransitionFromTop
+        case 3:
+            transition.subtype = kCATransitionFromBottom
+        default:
+            break
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func switchImage() {
+//        let transition = CATransition()
+        
+//        transition.type = kCATransitionMoveIn
+        imageView.layer.add(transition, forKey: nil)
+        
+        guard let currentImage = imageView.image,
+        var index = imageArray?.index(of: currentImage)
+        else { return }
+        
+        index = (index + 1) % (imageArray?.count ?? 1)
+        
+        imageView.image = imageArray?[index]
     }
 
 
