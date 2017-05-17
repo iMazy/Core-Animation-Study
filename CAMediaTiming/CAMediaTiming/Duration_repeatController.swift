d4a589067a1536027a6a2c6b9cf2c678caea01ed
Mazy <mazy_ios@163.com>
2017-05-17 10:02:31 +0800

    feat: 初始化界面

---
+++ CAMediaTiming/CAMediaTiming/Duration_repeatController.swift
@@ -0,0 +1,35 @@
+//
+//  Duration_repeatController.swift
+//  CAMediaTiming
+//
+//  Created by Mazy on 2017/5/17.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class Duration_repeatController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
